f5b463c2a32cbe63030af15675b870df569ea79d
Mateo Difranco <mateodifr@gmail.com>
2022-09-27 23:22:06 -0300

    Optimizar Set

--- tp-5/Set.hs
+++ tp-5/Set.hs
@@ -6,28 +6,26 @@ data Set a = S [a] Int
 emptyS :: Set a
 emptyS = S [] 0
 
--- Dados un elemento y un conjunto, agrega el elemento al conjunto.
-addS :: Eq a => a -> Set a -> Set a
-addS a (S xs l) = S (a : xs) (l + 1)
-
--- Dados un elemento y un conjunto indica si el elemento pertenece al conjunto.
 pertenece :: Eq a => a -> [a] -> Bool
 pertenece _ [] = False
 pertenece a (x:xs) = a == x || pertenece a xs
 
+-- Dados un elemento y un conjunto, agrega el elemento al conjunto.
+agregar :: Eq a => a -> [a] -> [a]
+agregar a xs = if pertenece a xs then xs else a : xs
+
+addS :: Eq a => a -> Set a -> Set a
+addS a (S xs l) =
+  let agregado = agregar a xs
+  in S agregado (length agregado)
+
+-- Dados un elemento y un conjunto indica si el elemento pertenece al conjunto.
 belongs :: Eq a => a -> Set a -> Bool
 belongs a (S xs _) = pertenece a xs
 
 -- Devuelve la cantidad de elementos distintos de un conjunto.
-cantidadDeUnicos :: Eq a => [a] -> Int
-cantidadDeUnicos [] = 0
-cantidadDeUnicos (x:xs) =
-  if pertenece x xs
-    then cantidadDeUnicos xs
-    else 1 + cantidadDeUnicos xs
-
 sizeS :: Eq a => Set a -> Int
-sizeS (S xs _) = cantidadDeUnicos xs
+sizeS (S _ l) = l
 
 -- Borra un elemento del conjunto.
 sacar :: Eq a => a -> [a] -> [a]
@@ -41,17 +39,17 @@ removeS :: Eq a => a -> Set a -> Set a
 removeS a (S xs l) = S (sacar a xs) (l - 1)
 
 -- Dados dos conjuntos devuelve un conjunto con todos los elementos de ambos. conjuntos.
-appendSinRepetidos :: Eq a => [a] -> [a] -> [a]
-appendSinRepetidos [] ys = ys
-appendSinRepetidos (x:xs) ys =
+unir :: Eq a => [a] -> [a] -> [a]
+unir [] ys = ys
+unir (x:xs) ys =
   if pertenece x ys
-    then appendSinRepetidos xs ys
-    else x : appendSinRepetidos xs ys
+    then unir xs ys
+    else x : unir xs ys
 
 unionS :: Eq a => Set a -> Set a -> Set a
 unionS (S xs _) (S ys _) =
-  let union = appendSinRepetidos xs ys
-  in S union (cantidadDeUnicos union)
+  let union = unir xs ys
+  in S union (length union)
 
 -- Dado un conjunto devuelve una lista con todos los elementos distintos del conjunto.
 setToList :: Eq a => Set a -> [a]
